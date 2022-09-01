a1dcbfe4b03b702c7a4e78b5e238519f232a11cd
Nastua3505 <nastuaaas@mail.ru>
2022-09-01 16:39:02 +0300

    addNew

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [text,setText]= useState("")
+    const [todos,setTodo]= useState([
+   
+    ]);
+    
+    
+    const addTodo=()=> {
+      const newArr=[ ...todos, {id: todos.length+1,text:text}];
+      setTodo(newArr)
+      setText("");
+    };
+
+    const deleteTodo=(id)=>{
+      const newArr = todos.filter((item)=> item.id !== id);
+      setTodo(newArr);
+    }   
+
+  
+    return (
+      <div className="app">
+        < div className="container">
+        <h1>What's the plan for today?</h1>
+        <input onChange={(e)=>setText(e.target.value)} value={text}/>
+        <button onClick={()=> addTodo()}>add Todo</button>
+    
+  
+        {todos.map((item) =>(
+          <div className='prop' key={item.id} style={{backgroundColor:""}}>
+            <div>
+            <p>{item.text}</p></div>
+            <div className='footer'>
+             <button onClick={deleteTodo}>delite</button>
+             <button>edit</button>
+          
+            </div>
+            </div>
+        ))}
+
+        </div>
+        </div>
+        )
+  };
+
 
 export default App;
